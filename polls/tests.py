0cfe07868704599bd78c7cdd5dce7f646d1a2bde
Wilson Delgado <wilson.delgadoher@gmail.com>
2022-04-12 14:58:32 -0500

    Adding test to method of model Question

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+# Python
+import datetime
 
-# Create your tests here.
+# Django
+from django.test import TestCase
+from django.utils import timezone
+from .models import Question
+class QuestionModelTest(TestCase):
+    def test_was_published_recently_with_future_questions(self):
+        """was_published_recently returns false for questions whose pub_date is in future"""
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_questions = Question(question_text="Who is the best course director at platzi?",pub_date=time)
+        self.assertEquals(future_questions.was_published_recently(), False)
