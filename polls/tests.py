80b73ccbc0f6737d764ace605a67f707b952165d
Wilson Delgado <wilson.delgadoher@gmail.com>
2022-04-12 21:59:17 -0500

    Adding tests for was_published_recently method in Question

--- polls/tests.py
+++ polls/tests.py
@@ -11,3 +11,15 @@ class QuestionModelTest(TestCase):
         time = timezone.now() + datetime.timedelta(days=30)
         future_questions = Question(question_text="Who is the best course director at platzi?",pub_date=time)
         self.assertEquals(future_questions.was_published_recently(), False)
+
+    def test_was_published_recently_with_present_questions(self):
+        """was_published_recently returns true for questions whose pub_date is in the current time"""
+        time = timezone.now()
+        present_questions = Question(question_text="Who is the teacher at platzi?",pub_date=time)
+        self.assertEquals(present_questions.was_published_recently(), True)
+
+    def test_was_published_recently_with_past_questions(self):
+        """was_published_recently returns false for questions whose pub_date is in the past"""
+        time = timezone.now() - datetime.timedelta(days=2)
+        present_questions = Question(question_text="Who is the teacher at platzi?",pub_date=time)
+        self.assertEquals(present_questions.was_published_recently(), False)
