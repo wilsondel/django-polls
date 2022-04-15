9deb1b8fa86cdb49ae703770a0006bca2f2c9c1d
Wilson Delgado <wilson.delgadoher@gmail.com>
2022-04-14 22:31:58 -0500

    improving_django_admin

--- polls/admin.py
+++ polls/admin.py
@@ -2,6 +2,20 @@
 from django.contrib import admin
 
 # Models
-from polls.models import Question
+from polls.models import Choice, Question
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ["pub_date","question_text"]
+    inlines = [ChoiceInline]
+    list_display = ("question_text","pub_date","mod_date","was_published_recently")
+    list_filter = ["pub_date","mod_date"]
+    search_fields = ["question_text"]
+
+
+# admin.site.register(Question, QuestionAdmin) => @admin.register(Question)
--- polls/models.py
+++ polls/models.py
@@ -4,15 +4,22 @@ import datetime
 # Django
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 
 class Question(models.Model):
     question_text = models.CharField(max_length=250)
-    pub_date = models.DateTimeField(auto_now_add=True)
+    pub_date = models.DateTimeField()
     mod_date = models.DateTimeField(auto_now=True)
+
 
     def __str__(self):
         return self.question_text
-
+    
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Published recently?',
+    )
     def was_published_recently(self):
         return timezone.now() >= self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
@@ -20,7 +27,7 @@ class Question(models.Model):
 class Choice(models.Model):
     fk_question = models.ForeignKey(Question,on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=250)
-    votes = models.IntegerField()
+    votes = models.IntegerField(default=0)
 
     def __str__(self):
         return self.choice_text
